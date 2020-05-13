aa667522a1e3d022811c99ceafeb14d082ac402c
noor786838 <63953012+noor786838@users.noreply.github.com>
2020-05-13 14:14:42 +0530

    we need a size na
    to orint size of data types

---
+++ let.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int size;
+	cout<<"sizeof int\t"<<sizeof(int)<<endl;
+	cout<<"sizeof char\t"<<sizeof (char)<<endl;
+	cout<<"sizeof double\t"<<sizeof(double)<<endl;
+	cout<<"sizeof float\t"<<sizeof(float)<<endl;
+	return 0;
+}
